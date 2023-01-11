da206e4ed929996ad5fef3bd44cbe9345fcf6625
mosesochacha <ochachamoses443@gmail.com>
2023-01-11 14:00:49 +0300

    Complite

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,13 @@ import React from "react";
 import  image from "../data/data";
 
 function About() {
-  return <div id="about">
+  return( <div id="about">
     <h2>About Me</h2>
-      <img src={image} alt="I made this"/>
-  </div>;
+    <p>I made this</p>
+     <img src={image} alt="I made this"/>
+
+  </div>
+  )
 
 }
 
